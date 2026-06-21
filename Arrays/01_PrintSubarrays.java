cd2e12ac98898b141a0e70916204a25f393b95a2
Babul Singh <babulsingh8051@gmail.com>
2026-06-22 00:53:01 +0530

    Add Print Subarrays solution
    Implemented a method to print all subarrays of a given array.

---
+++ Arrays/01_PrintSubarrays.java
@@ -0,0 +1,24 @@
+// Problem: Print All Subarrays
+// Topic: Arrays
+// Time Complexity: O(n^3)
+// Space Complexity: O(1)
+
+public class PrintSubarrays {
+
+    public static void printSubArrays(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        printSubArrays(numbers);
+    }
+}
