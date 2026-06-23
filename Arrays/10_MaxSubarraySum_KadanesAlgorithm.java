3d3ff6fee51959ae0a93ada2dd2c55ce99f7e30b
Babul Singh <babulsingh8051@gmail.com>
2026-06-23 23:08:05 +0530

    Add Kadane's Algorithm for maximum subarray sum
    Implement Kadane's Algorithm to find maximum subarray sum.

---
+++ Arrays/10_MaxSubarraySum_KadanesAlgorithm.java
@@ -0,0 +1,32 @@
+/*
+ * Problem: Maximum Subarray Sum using Kadane's Algorithm
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class Q09_MaxSubarraySum_KadanesAlgorithm {
+
+    public static void kadanes(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            currSum += numbers[i];
+
+            maxSum = Math.max(maxSum, currSum);
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5, 6};
+
+        kadanes(numbers);
+    }
+}
